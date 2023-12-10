2c571210f0ecaa8c3ba621a5d75785171434ffbb
Pawan608 <agarwalpawan6089@gmail.com>
2023-12-11 00:42:05 +0530

    new

--- dist/main.js
+++ dist/main.js
@@ -5,15 +5,16 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const dotenv = require("dotenv");
 dotenv.config();
+console.log("server running on", process.env.PORT, process.env.POSTGRES_URL);
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.useGlobalPipes(new common_1.ValidationPipe());
     app.enableCors({
-        origin: ["http://localhost:5174", "http://localhost:3001"],
+        origin: ["http://localhost:5173", "http://localhost:3001"],
         methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
         credentials: true,
     });
-    await app.listen(process.env.PORT, "0.0.0.0");
+    await app.listen(process.env.PORT);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
