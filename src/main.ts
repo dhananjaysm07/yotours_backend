214aed416d5d43692cc0c0202c7d51fc0d6b6797
Pawan608 <agarwalpawan6089@gmail.com>
2023-12-10 17:26:19 +0530

    server whitelist

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
-dotenv.config(); 
+import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import * as dotenv from "dotenv";
+dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: ['http://localhost:5173','http://localhost:3001'], // Specify the frontend origin
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
+    origin: ["http://localhost:5174", "http://localhost:3001"], // Specify the frontend origin
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Allowed HTTP methods
     credentials: true, // This allows session cookies to be sent back and forth
   });
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.PORT, "0.0.0.0");
 }
 bootstrap();
