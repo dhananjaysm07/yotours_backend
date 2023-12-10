c6923c0cd83114c7189706fdd382449e53170446
Pawan608 <agarwalpawan6089@gmail.com>
2023-12-10 21:58:02 +0530

    new server

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import * as dotenv from "dotenv";
 dotenv.config();
+console.log("server running on", process.env.PORT, process.env.POSTGRES_URL);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
