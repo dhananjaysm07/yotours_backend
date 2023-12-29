579a88a46865246bbc0e3e3ebc23ae032f987cd6
Pawan608 <agarwalpawan6089@gmail.com>
2023-12-30 03:04:32 +0530

    create genral package

--- dist/package/dto/create-package.input.d.ts
+++ dist/package/dto/create-package.input.d.ts
@@ -1,11 +1,13 @@
 import { HighlightInput } from "./highlight.input";
 import { PhotoInput } from "./photo.input";
 export declare class CreatePackageGeneralInput {
-    title: string;
-    type: string;
-    summary: string;
-    currentStep: number;
-    destinationIds: string[];
-    highlights: HighlightInput[];
-    photos: PhotoInput[];
+  title: string;
+  type: string;
+  summary: string;
+  currentStep: number;
+  destinationIds: string[];
+  highlights: HighlightInput[];
+  photos: PhotoInput[];
+  createdAt: Date;
+  updatedAt: Date;
 }
--- dist/package/package.service.d.ts
+++ dist/package/package.service.d.ts
@@ -9,23 +9,42 @@ import { LocationDetails } from "./entities/location.entity";
 import { LocationDetailsInput } from "./dto/location.input";
 import { DestinationService } from "src/destination/destination.service";
 export declare class PackageGeneralService {
-    private dataSource;
-    private packageGeneralRepository;
-    private locationDetailsRepository;
-    private highlightRepository;
-    private photoRepository;
-    private itineraryRepository;
-    private destinationService;
-    constructor(dataSource: DataSource, packageGeneralRepository: Repository<PackageGeneral>, locationDetailsRepository: Repository<LocationDetails>, highlightRepository: Repository<Highlight>, photoRepository: Repository<Photo>, itineraryRepository: Repository<Itinerary>, destinationService: DestinationService);
-    create(createPackageGeneralInput: CreatePackageGeneralInput): Promise<PackageGeneral>;
-    delete(id: string): Promise<string>;
-    findOne(id: string): Promise<PackageGeneral>;
-    addItineraries(id: string, itineraryInputs: ItineraryInput[]): Promise<string[]>;
-    addLocationDetails(id: string, locationDetailsInput: LocationDetailsInput): Promise<LocationDetails>;
-    findAll(page?: number, limit?: number): Promise<{
-        items: PackageGeneral[];
-        totalCount: number;
-    }>;
-    findOneWithItineraries(id: string): Promise<PackageGeneral>;
-    findOneWithLocation(id: string): Promise<PackageGeneral>;
+  private dataSource;
+  private packageGeneralRepository;
+  private locationDetailsRepository;
+  private highlightRepository;
+  private photoRepository;
+  private itineraryRepository;
+  private destinationService;
+  constructor(
+    dataSource: DataSource,
+    packageGeneralRepository: Repository<PackageGeneral>,
+    locationDetailsRepository: Repository<LocationDetails>,
+    highlightRepository: Repository<Highlight>,
+    photoRepository: Repository<Photo>,
+    itineraryRepository: Repository<Itinerary>,
+    destinationService: DestinationService
+  );
+  create(
+    createPackageGeneralInput: CreatePackageGeneralInput
+  ): Promise<PackageGeneral>;
+  delete(id: string): Promise<string>;
+  findOne(id: string): Promise<PackageGeneral>;
+  addItineraries(
+    id: string,
+    itineraryInputs: ItineraryInput[]
+  ): Promise<string[]>;
+  addLocationDetails(
+    id: string,
+    locationDetailsInput: LocationDetailsInput
+  ): Promise<LocationDetails>;
+  findAll(
+    page?: number,
+    limit?: number
+  ): Promise<{
+    items: PackageGeneral[];
+    totalCount: number;
+  }>;
+  findOneWithItineraries(id: string): Promise<PackageGeneral>;
+  findOneWithLocation(id: string): Promise<PackageGeneral>;
 }
