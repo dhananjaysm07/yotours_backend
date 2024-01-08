1d47e51033915d3e07d94480a3805e5b24d6b2c6
Pawan608 <agarwalpawan6089@gmail.com>
2024-01-08 11:47:55 +0530

    new

--- src/config/database.module.ts
+++ src/config/database.module.ts
@@ -12,26 +12,28 @@ import { ConfigModule, ConfigService } from "@nestjs/config";
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
         type: "postgres",
-        url: "postgres://issue_user:k0BgaH5w44to15qIhQWRu6SKVhEhlWZT@dpg-ckmdd8gu1l6c7383q9jg-a.oregon-postgres.render.com/issues_database",
-        host: configService.get<string>(
-          "DB_HOST",
-          "dpg-ckffbhunpffc73f603ig-a"
-        ), // Replace with your actual environment variable names
-        port: configService.get<number>("DB_PORT", 5432),
-        username: configService.get<string>("DB_USERNAME", "jira_clone_user"),
-        password: configService.get<string>(
-          "DB_PASSWORD",
-          "FraeZh9zOgJvt1fRLpU08it9tsKJAvNo"
-        ),
-        database: configService.get<string>("DB_DATABASE", "jira_clone"),
-        // host: configService.get<string>("DB_HOST", "localhost"), // Replace with your actual environment variable names
+        // url: "postgres://issue_user:k0BgaH5w44to15qIhQWRu6SKVhEhlWZT@dpg-ckmdd8gu1l6c7383q9jg-a.oregon-postgres.render.com/issues_database",
+        // host: configService.get<string>(
+        //   "DB_HOST",
+        //   "dpg-ckffbhunpffc73f603ig-a"
+        // ), // Replace with your actual environment variable names
         // port: configService.get<number>("DB_PORT", 5432),
-        // username: configService.get<string>("DB_USERNAME", "postgres"),
-        // password: configService.get<string>("DB_PASSWORD", "mypassword"),
-        // database: configService.get<string>("DB_DATABASE", "yotours"),
+        // username: configService.get<string>("DB_USERNAME", "jira_clone_user"),
+        // password: configService.get<string>(
+        //   "DB_PASSWORD",
+        //   "FraeZh9zOgJvt1fRLpU08it9tsKJAvNo"
+        // ),
+        // database: configService.get<string>("DB_DATABASE", "jira_clone"),
+        ///////////////////////////////////////////////////////////////
+        /////////////////////////////////////////////////////
+        host: configService.get<string>("DB_HOST", "localhost"), // Replace with your actual environment variable names
+        port: configService.get<number>("DB_PORT", 5432),
+        username: configService.get<string>("DB_USERNAME", "postgres"),
+        password: configService.get<string>("DB_PASSWORD", "mypassword"),
+        database: configService.get<string>("DB_DATABASE", "yotours"),
         entities: ["dist/**/*.entity{.ts,.js}"],
         synchronize: true,
-        ssl: true,
+        ssl: false,
       }),
       inject: [ConfigService],
     }),
